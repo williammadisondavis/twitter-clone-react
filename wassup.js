f674cc4fa97602fc0f862af0f84fdf7fbb74d530
Will Davis <williammadisondavis@gmail.com>
2018-09-14 14:19:09 -0400

    working-react-site

--- wassup.js
+++ wassup.js
@@ -8,34 +8,40 @@ class HomePage extends React.Component {
             pageTitle: 'Wassap',
             tweets: [
                 {
-                    id: 0, user: 'Will', body: 'afejafkl'
+                    id: 0, user: 'Will', body: ' react is g'
                 },
                 {
                     id: 1, user: 'Timmy', body: 'lakjefkhae;kfh;aehfk'
                 },
             ]
-        };       
+        };
     }
 
     render() {
         const { tweets } = this.state;
-        let deletePost = () => {
-            console.log('click')
+
+        let deletePost = (userID) => {
+            let updatedState = tweets.filter(post => {
+                return post.id !== userID
+            })
+            // console.log(updatedState)
+            this.setState({tweets: updatedState})
         }
+        
         let addPost = (post) => {
             let newState = [...tweets, post];
-            this.setState({tweets: newState})
+            this.setState({ tweets: newState })
         }
         return (
 
-            h('div', {}, 
+            h('div', {},
 
                 h('h1', { className: 'main-header' }, "Wassap"),
-                h(FormDocument, {addPost}),
+                h(FormDocument, { addPost }),
                 h(DisplayPostList, { posts: tweets, deletePost: deletePost }),
                 h('footer', {}, ['Developed by Will Davis']),
                 h('a', { href: 'williamdav.is' }, ['My Website'])
-            
+
             ))
     }
 }
@@ -45,50 +51,67 @@ class FormDocument extends React.Component {
         super(props);
         this.state = {
             user: '',
-            newWassap: ''
+            body: ''
         }
     }
     render() {
-        const {addPost} = this.props;
+        const { addPost } = this.props;
+        const {user, body} = this.state;
+        let tweetBox = (event, updated) => {
+            let newPost = {}
+            newPost[updated] = event.target.value;
+            this.setState(newPost);
+        };
+        let submitForm = (event) => {
+            event.preventDefault();
+            addPost(this.state)
+
+        }
         return (
-            h('form', {
-                onSubmit: (event) => {
-                    // event.preventDefault();
-                    // console.log(event);
-                    // this.state.newWassap
-                    // console.log('submit!!!');
-                }
-            },
-            [ 
-                h('input', {
-                    type: 'text',
-                    value: this.state.newWassap,
-                    onChange: (event) => {
-                        let value = event.target.value
-                        console.log(value);
-                        this.setState({newWassap: value})
-                    }
-            }),
-                h('input', { type: 'submit', value: 'Post'}),
-            ]
-            )
-        )
+            <form onSubmit={submitForm}>
+                <input type='text' value={user} onChange={(event)=>{tweetBox(event, 'user')}}/>
+                <input type='text' value={body} onChange={(event)=>{tweetBox(event, 'body')}}/>
+                <button type='submit'>Click me</button>
+            </form>
+        )}
     }
+
+
+
+let DisplayPostList = ({ posts, deletePost }) => {
+    return (
+        <div>
+            {posts.map(userPost => 
+             (
+                <ul key={userPost.id}>
+                    {<PostRow
+                    post={userPost}
+                    deletePost={deletePost}
+                    />}
+                </ul>
+            ))}
+        </div>
+    )
+}
+{/* // return h('ul', null, posts.map(userPost => 
+    //     // console.log(userPost);
+    //     return h(postRow, { post: userPost, deletePost: userPost.deletePost })
+    // })) */}
+
+let PostRow = ({ post, deletePost }) => {
+    // console.log(post);
+    return (
+        <li> {[post.body, post.user]}
+            <button onClick={() => deletePost(post.id)}>
+            Delete Post
+            </button>
+        </li>
+        
+    )
 }
 
-let DisplayPostList = ({posts}) => {
-    return h('ul', null, posts.map(userPost => {
-        // console.log(userPost);
-        return h(postRow, { post: userPost, key: userPost.id, deletePost: userPost.deletePost})
-    }))
-}
-
-let postRow = ({post, deletePost}) => {
-    console.log(post);
-    return h('li', null, [post.body, post.user], h('button', { onClick: () => deletePost.deletePost()}, 'Delete'))
-
-}
+// (<li> [post.body, post.user] </li>,
+//     <button onClick={ () => deletePost.deletePost() }/>)
 
 
-
-ReactDOM.render(h(HomePage), document.querySelector('.react-root'));
+ReactDOM.render(<HomePage/>, document.querySelector('.react-root'));
