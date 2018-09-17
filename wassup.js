67fc1988197bdf49d6be6ae981803e1ca411918a
Will Davis <williammadisondavis@gmail.com>
2018-09-17 11:34:01 -0400

    added-fetch-to-tweet-api

--- wassup.js
+++ wassup.js
@@ -1,21 +1,17 @@
-const h = React.createElement;
-
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: '',
-            pageTitle: 'Wassap',
-            tweets: [
-                {
-                    id: 0, user: 'Will', body: ' react is g'
-                },
-                {
-                    id: 1, user: 'Timmy', body: 'lakjefkhae;kfh;aehfk'
-                },
-            ]
+            tweets: []
         };
     }
+    componentDidMount() {
+        console.log('hey')
+        fetch('http://0.tcp.ngrok.io:11971/wassups.json')
+          .then(response => response.json())
+          .then(data => 
+            this.setState({tweets: data}))
+    };
 
     render() {
         const { tweets } = this.state;
@@ -24,7 +20,6 @@ class HomePage extends React.Component {
             let updatedState = tweets.filter(post => {
                 return post.id !== userID
             })
-            // console.log(updatedState)
             this.setState({tweets: updatedState})
         }
         
@@ -33,16 +28,14 @@ class HomePage extends React.Component {
             this.setState({ tweets: newState })
         }
         return (
-
-            h('div', {},
-
-                h('h1', { className: 'main-header' }, "Wassap"),
-                h(FormDocument, { addPost }),
-                h(DisplayPostList, { posts: tweets, deletePost: deletePost }),
-                h('footer', {}, ['Developed by Will Davis']),
-                h('a', { href: 'williamdav.is' }, ['My Website'])
-
-            ))
+            <div>
+                <h1 className={'main-header'}>Wassap</h1>
+                {<FormDocument addPost={ addPost }/>}
+                {<DisplayPostList posts={ tweets } deletePost={deletePost}/>}
+                <footer>Developer by Will Davis</footer>
+                <a href="williamdav.is">My Website</a>
+            </div>
+        )
     }
 }
 
@@ -51,12 +44,12 @@ class FormDocument extends React.Component {
         super(props);
         this.state = {
             user: '',
-            body: ''
+            content: ''
         }
     }
     render() {
         const { addPost } = this.props;
-        const {user, body} = this.state;
+        const {user, content} = this.state;
         let tweetBox = (event, updated) => {
             let newPost = {}
             newPost[updated] = event.target.value;
@@ -70,7 +63,7 @@ class FormDocument extends React.Component {
         return (
             <form onSubmit={submitForm}>
                 <input type='text' value={user} onChange={(event)=>{tweetBox(event, 'user')}}/>
-                <input type='text' value={body} onChange={(event)=>{tweetBox(event, 'body')}}/>
+                <input type='text' value={content} onChange={(event)=>{tweetBox(event, 'content')}}/>
                 <button type='submit'>Click me</button>
             </form>
         )}
@@ -93,15 +86,11 @@ let DisplayPostList = ({ posts, deletePost }) => {
         </div>
     )
 }
-{/* // return h('ul', null, posts.map(userPost => 
-    //     // console.log(userPost);
-    //     return h(postRow, { post: userPost, deletePost: userPost.deletePost })
-    // })) */}
 
 let PostRow = ({ post, deletePost }) => {
     // console.log(post);
     return (
-        <li> {[post.body, post.user]}
+        <li> {[ post.user, post.content]}
             <button onClick={() => deletePost(post.id)}>
             Delete Post
             </button>
@@ -109,9 +98,5 @@ let PostRow = ({ post, deletePost }) => {
         
     )
 }
-
-// (<li> [post.body, post.user] </li>,
-//     <button onClick={ () => deletePost.deletePost() }/>)
-
 
 ReactDOM.render(<HomePage/>, document.querySelector('.react-root'));
